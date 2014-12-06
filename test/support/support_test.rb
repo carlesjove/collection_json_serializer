ced6c3e819165040c07f49f7bf950cab551dfbb8
Carles Jove i Buxeda <hola@carlus.cat>
2014-12-06 01:11:18 +0100

    added TODO support_test

---
+++ test/support/support_test.rb
@@ -0,0 +1,11 @@
+require 'minitest_helper'
+
+module CollectionJsonSerializer
+  module Support
+    class TestSupport < Minitest::Test
+      def test_extract_value
+        skip
+      end
+    end
+  end
+end
