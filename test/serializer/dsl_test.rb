4adb7a2705340014e985571fd9a81d057e9017f0
Carles Jove i Buxeda <hola@carlus.cat>
2015-01-11 21:54:37 +0100

    unify dsl test into one file

---
+++ test/serializer/dsl_test.rb
@@ -0,0 +1,27 @@
+require "minitest_helper"
+
+module CollectionJson
+  class Serializer
+    class TestDSL < Minitest::Test
+      def setup
+        @user = User.new(name: "Carles Jove", email: "hola@carlus.cat")
+        @serializer = Serializer.new(@user)
+      end
+
+      def test_top_level_dsl
+        assert @serializer.respond_to?(:href)
+        assert @serializer.respond_to?(:links)
+        assert @serializer.respond_to?(:template)
+        assert @serializer.respond_to?(:queries)
+        assert @serializer.respond_to?(:items)
+      end
+
+      def test_items_dsl
+        assert_equal Items, @serializer.items.class
+        assert @serializer.items.respond_to?(:attributes)
+        assert @serializer.items.respond_to?(:links)
+        assert @serializer.items.respond_to?(:href)
+      end
+    end
+  end
+end
