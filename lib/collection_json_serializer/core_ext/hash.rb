3dbc8778573b8c0144cff228017533f4901c7e29
Carles Jove i Buxeda <hola@carlus.cat>
2015-01-12 09:53:26 +0100

    added documentation to extract_params

--- lib/collection_json_serializer/core_ext/hash.rb
+++ lib/collection_json_serializer/core_ext/hash.rb
@@ -1,4 +1,11 @@
 class Hash
+  # Extract params
+  # Returns a two-key hash where the first key is :name and the second is
+  # :properties, being the latter a hash itself.
+  #
+  # hash = { hello: { prompt: "The prompt", value: "Hello World"" } }
+  # hash.extract_params
+  # => { name: "hello", properties: { prompt: "The prompt", value: "Hello World" } }
   def extract_params
     params = {}
     params[:name] = keys.first
