2ef83585778909b2e1d272b8f382fb9774b87bb5
Carles Jove i Buxeda <hola@carlus.cat>
2015-03-02 14:16:11 +0100

    get rid of valid serializer cause it's stupid

--- test/fixtures/serializers/valid_serializer.rb
+++
@@ -1,5 +0,0 @@
-class ValidSerializer < CollectionJson::Serializer
-  href self: "http://example.com/users/1",
-       collection: "http://example.com/users"
-  link dashboard: { href: "http://example.com/my-dashboard" }
-end
