a7b399152cdf6afe0d93d97be1d0c17731f82b15
Carles Jove i Buxeda <hola@carlus.cat>
2015-03-04 12:57:04 +0100

    use an empty serializer instead. closes #47

--- test/validator/extensions_validation_test.rb
+++ test/validator/extensions_validation_test.rb
@@ -10,25 +10,28 @@ module CollectionJson
   class Serializer
     class Validator
       class TestExtensionsValidation < Minitest::Test
+        include TestHelper
+
         def setup
           @user = User.new(
             name: "Carles Jove",
             email: "hola@carlus.cat"
           )
-          @serializer = UserSerializer.new(@user)
         end
 
         def test_that_unknown_extensions_produce_an_error
-          @serializer.class._extensions = [:unknown_extension]
+          serializer = empty_serializer_for(@user)
+          serializer.class._extensions = [:non_existing_extension]
 
-          assert @serializer.invalid?, "serializer should be invalid"
-          assert @serializer.errors.include?(:extensions)
+          assert serializer.invalid?, "serializer should be invalid"
+          assert serializer.errors.include?(:extensions)
         end
 
         def test_that_known_extensions_dont_producte_an_error
-          @serializer.class._extensions = [:existing_extension]
+          serializer = empty_serializer_for(@user)
+          serializer.class._extensions = [:existing_extension]
 
-          assert @serializer.valid?, "serializer should be valid"
+          assert serializer.valid?, "serializer should be valid"
         end
 
         def test_that_an_extension_with_no_definition_raises_proper_error
